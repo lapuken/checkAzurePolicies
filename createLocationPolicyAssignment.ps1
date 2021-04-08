1cce031690b64c5bcb90aba8356a236d5682450b
lemwish <mcdonald.am97@gmail.com>
2021-04-08 10:41:27 -0400

    Add location policy

---
+++ createLocationPolicyAssignment.ps1
@@ -0,0 +1,16 @@
+# Get a reference to the Management group that is the scope of the assignment
+
+$Authorizedlocation = "EastUS"
+$mg = Get-AzManagementGroup | Where-Object  {$_.Name -eq "Contoso"}
+$policyparam = '{ "listOfAllowedLocations": { "value": ["EastUS" ] } }'
+
+# Get a reference to the built-in policy definition to assign
+$definition = Get-AzPolicyDefinition | Where-Object { $_.Properties.DisplayName -eq 'Allowed locations' }
+
+# Create the policy assignment with the built-in definition against your management group
+New-AzPolicyAssignment -Name 'allowed-locations' `
+                       -DisplayName 'Deny resource deployment outside WestUS' `
+                       -Scope $mg.Id `
+                       -PolicyDefinition $definition `
+                       -PolicyParameter $policyparam
+
---
+++ createnumngmentgrp.ps1
@@ -0,0 +1,6 @@
+#Creare new management group
+
+New-AzManagementGroup -GroupName 'Contoso' -DisplayName 'Contoso Group'
+
+#remove management group
+#Remove-AzManagementGroup -GroupName 'Contoso'
