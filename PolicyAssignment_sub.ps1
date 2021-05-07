811a69b9fd33e464f0be7444bafddb50d6977c95
lemwish <mcdonald.am97@gmail.com>
2021-05-07 09:57:41 -0400

    Add Parameters to Subs

---
+++ PolicyAssignment_sub.ps1
@@ -0,0 +1,124 @@
+# Get a reference to the Management group that is the scope of the assignment
+
+######### Policies for Subscription sub-cms-IT
+
+$mg = Get-AzSubscription | Where-Object  {$_.Name -LIKE "sub-cms-IT"}
+$mg = "/subscriptions/$($mg.Id)"
+$policyparam = '{ "listOfAllowedLocations": { "value": ["EastUS"] } }'
+
+# Get a reference to the built-in policy definition to assign
+$definition = Get-AzPolicyDefinition | Where-Object { $_.Properties.DisplayName -eq 'Allowed locations' }
+
+# Create the policy assignment with the built-in definition against your management group
+New-AzPolicyAssignment -Name 'allowed-locations' `
+                       -DisplayName 'Deny resource deployment outside EastUS' `
+                       -Scope $mg `
+                       -PolicyDefinition $definition `
+                       -PolicyParameter $policyparam
+
+# Kubernetes cluster containers should only use allowed images
+
+# Get a reference to the built-in policy definition to assign
+$definition = Get-AzPolicyDefinition | Where-Object { $_.Properties.DisplayName -eq 'Kubernetes cluster containers should only use allowed images' }
+$policyparam = '{ "allowedContainerImagesRegex" : { "value": "^khido$" }}'
+
+# Create the policy assignment with the built-in definition against your management group
+New-AzPolicyAssignment -Name 'Allowed images' `
+                       -DisplayName 'Kubernetes cluster containers should only use allowed images' `
+                       -Scope $mg `
+                       -PolicyDefinition $definition `
+                       -PolicyParameter $policyparam
+
+##### # Kubernetes cluster containers should only listen on allowed ports
+
+# Get a reference to the built-in policy definition to assign
+$definition = Get-AzPolicyDefinition | Where-Object { $_.Properties.DisplayName -eq 'Kubernetes cluster containers should only listen on allowed ports' }
+$policyparam = '{ "allowedContainerPortsList" : { "value": [10,1001] }}'
+
+# Create the policy assignment with the built-in definition against your management group
+New-AzPolicyAssignment -Name 'Allowed container ports' `
+                       -DisplayName 'Kubernetes cluster containers should only use Allowed container ports' `
+                       -Scope $mg `
+                       -PolicyDefinition $definition `
+                       -PolicyParameter $policyparam
+
+
+##### # Kubernetes cluster services should listen only on allowed ports
+
+# Get a reference to the built-in policy definition to assign
+$definition = Get-AzPolicyDefinition | Where-Object { $_.Properties.DisplayName -eq 'Kubernetes cluster services should listen only on allowed ports' }
+$policyparam = '{ "allowedServicePortsList" : { "value": [10,1001] }}'
+
+# Create the policy assignment with the built-in definition against your management group
+New-AzPolicyAssignment -Name 'Allowed Service ports' `
+                       -DisplayName 'Kubernetes cluster services should listen only on allowed ports' `
+                       -Scope $mg `
+                       -PolicyDefinition $definition `
+                       -PolicyParameter $policyparam
+
+
+######### Policies for Subscription headquarters-dev-001
+
+$mg = Get-AzSubscription | Where-Object  {$_.Name -LIKE "headquarters-dev-001"}
+$mg = "/subscriptions/$($mg.Id)"
+
+$policyparam = '{ "listOfAllowedLocations": { "value": ["EastUS"] } }'
+
+# Get a reference to the built-in policy definition to assign
+$definition = Get-AzPolicyDefinition | Where-Object { $_.Properties.DisplayName -eq 'Allowed locations' }
+
+# Create the policy assignment with the built-in definition against your management group
+New-AzPolicyAssignment -Name 'allowed-locations' `
+                       -DisplayName 'Deny resource deployment outside EastUS' `
+                       -Scope $mg `
+                       -PolicyDefinition $definition `
+                       -PolicyParameter $policyparam
+
+# Kubernetes cluster containers should only use allowed images
+
+# Get a reference to the built-in policy definition to assign
+$definition = Get-AzPolicyDefinition | Where-Object { $_.Properties.DisplayName -eq 'Kubernetes cluster containers should only use allowed images' }
+$policyparam = '{ "allowedContainerImagesRegex" : { "value": "^khido$" }}'
+
+# Create the policy assignment with the built-in definition against your management group
+New-AzPolicyAssignment -Name 'Allowed images' `
+                       -DisplayName 'Kubernetes cluster containers should only use allowed images' `
+                       -Scope $mg `
+                       -PolicyDefinition $definition `
+                       -PolicyParameter $policyparam
+
+##### # Kubernetes cluster containers should only listen on allowed ports
+
+# Get a reference to the built-in policy definition to assign
+$definition = Get-AzPolicyDefinition | Where-Object { $_.Properties.DisplayName -eq 'Kubernetes cluster containers should only listen on allowed ports' }
+$policyparam = '{ "allowedContainerPortsList" : { "value": [10,1001] }}'
+
+# Create the policy assignment with the built-in definition against your management group
+New-AzPolicyAssignment -Name 'Allowed container ports' `
+                       -DisplayName 'Kubernetes cluster containers should only use Allowed container ports' `
+                       -Scope $mg `
+                       -PolicyDefinition $definition `
+                       -PolicyParameter $policyparam
+
+
+##### # Kubernetes cluster services should listen only on allowed ports
+
+# Get a reference to the built-in policy definition to assign
+$definition = Get-AzPolicyDefinition | Where-Object { $_.Properties.DisplayName -eq 'Kubernetes cluster services should listen only on allowed ports' }
+$policyparam = '{ "allowedServicePortsList" : { "value": [10,1001] }}'
+
+# Create the policy assignment with the built-in definition against your management group
+New-AzPolicyAssignment -Name 'Allowed Service ports' `
+                       -DisplayName 'Kubernetes cluster services should listen only on allowed ports' `
+                       -Scope $mg `
+                       -PolicyDefinition $definition `
+                       -PolicyParameter $policyparam
+
+
+
+
+
+
+#Get-AzureRmPolicyDefinition | Select -ExpandProperty "Properties" | Where-Object { $_.DisplayName -like '*Kubernetes cluster containers should only use allowed images*' }
+#Get-AzureRmPolicyDefinition | Select -ExpandProperty "Properties" | Where-Object { $_.DisplayName -like '*Kubernetes cluster containers should only listen on allowed ports' }                       
+#Get-AzureRmPolicyDefinition | Select -ExpandProperty "Properties" | Where-Object { $_.DisplayName -like 'Kubernetes cluster services should listen only on allowed ports' }
